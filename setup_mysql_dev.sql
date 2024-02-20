044342b9c57a5d96e62eed0c104cd2c4db60349d
iDamina <daminailiya@gmail.com>
2024-02-20 02:09:38 +0100

    setup mysql dev

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- Preparing a MySQL server with the credentials specified for the project.
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON DB_NAME . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';

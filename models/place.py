159df5d67e3d78df9b2eba851394c23ab1bba432
Ayokayzy <theayokayzy1@gmail.com>
2024-02-22 04:35:25 +0000

    db

--- models/place.py
+++ models/place.py
@@ -5,6 +5,17 @@ from sqlalchemy import String, Column, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 
 
+place_amenity = Table(
+        'place_amenity', Base.metadata,
+        Column(
+            'place_id', String(60),
+            ForeignKey('places.id'),
+            nullable=False, primary_key=True),
+        Column(
+            'amenity_id', String(60),
+            ForeignKey('amenities.id'),
+            nullable=False, primary_key=True))
+
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
@@ -18,10 +29,25 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    amenity_ids = []
     reviews = relationship(
             'Review', backref='place', cascade="all, delete-orphan")
+    amenities = relationship(
+            'Amenity', secondary=place_amenity,
+            viewonly=False)
 
     @property
     def reviews(self):
         return [review for review in self.reviews
                 if review.place_id == self.id]
+
+    @property
+    def amenities(self):
+        return [amenity for amenity in self.amenities
+                if amenity.place_id == self.id]
+
+    @amenities.setter
+    def amenities(self, obj):
+        if type(obj).__name__ != 'Amenity':
+            return
+        self.amenity_ids.append(obj.id)
